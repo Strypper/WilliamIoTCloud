8508725881bc90578c3cf70d35afe6f0fa39b351
Strypper Vandel Jason <ocgrb.strypperjason115@gmail.com>
2020-08-12 20:38:44 +0700

    Version 1.0.0

---
+++ WilliamCloud/Controllers/TrashController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WilliamCloud.ObjectData;
+
+namespace WilliamCloud.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TrashController : ControllerBase
+    {
+        public TrashController()
+        {
+        }
+
+        [HttpPost]
+        public ActionResult UploadStatus([FromBody] TrashStatusDTO dto)
+        {
+            dto.SentTime = DateTime.Now;
+            return Ok(dto);
+        }
+    }
+}
---
+++ WilliamCloud/ObjectData/TrashStatusDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WilliamCloud.ObjectData
+{
+    public class TrashStatusDTO
+    {
+        public int TrashId { get; set; }
+        public string Model { get; set; }
+        public double Percent { get; set; }
+        public DateTime SentTime { get; set; }
+    }
+}
